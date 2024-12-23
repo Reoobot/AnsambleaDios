346d8c262396f45c43cfe21a831d90c84637c794
Reoobot <vegasfalexis@gmail.com>
2024-12-23 19:37:36 +0100

    changer in the time of de modal

--- scriptt.js
+++ scriptt.js
@@ -3,4 +3,4 @@ setTimeout(() => {
     const welcomeBox = document.getElementById('welcomeBox');
     welcomeBox.classList.add('animate__fadeOut');
     setTimeout(() => welcomeBox.style.display = 'none', 1000); 
-}, 5000);
+}, 3000);
